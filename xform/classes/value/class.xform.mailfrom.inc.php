75e197cbefc1b7b50e98684bf881e62c0b827ca7
jdlx <github@rexdev.de>
2012-03-27 02:56:05 +0200

    adapt 2.8

--- xform/classes/value/class.xform.mailfrom.inc.php
+++ xform/classes/value/class.xform.mailfrom.inc.php
@@ -7,28 +7,27 @@
 * @package redaxo4
 * @version Addon: 0.2
 * @version Klasse: 0.1
-*
-* $Id: class.xform.mailfrom.inc.php 69 2011-01-19 01:43:57Z jeffe $:
 */
 
 class rex_xform_mailfrom extends rex_xform_abstract
 {
 
-  function enterObject(&$email_elements,&$sql_elements,&$warning,&$form_output,$send = 0)
+  function enterObject()
   {
-
     // mailto als referenz auf anderes input feld (muss vor dem mailto feld stehen!)
-    if(isset($email_elements[$this->elements[1]]))
+    if(isset($this->params['value_pool']['email'][$this->getElement(1)]))
     {
-
-      $this->params["mail_from"] = $email_elements[$this->elements[1]];
-      $this->params["mail_from"] = str_replace(array("\n", "\r\n", "\r"), '', $this->params["mail_from"]);
-    }else
+      $this->params['mail_from'] = str_replace(
+        array('\n','\r'),
+        '',
+        $this->params['value_pool']['email'][$this->getElement(1)]
+        );
+    }
+    else
     {
       // direkt angegebene Emailadresse
-      $this->params["mail_from"] = $this->elements[1];
+      $this->params['mail_from'] = $this->getElement(1);
     }
-
   }
 
   function getDescription()
