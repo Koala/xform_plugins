7a9aa70a344900220fbcc4289b3c6d160f95d715
jdlx <github@rexdev.de>
2012-04-19 20:23:33 +0200

    2.8 fixes.. et. al.

--- xform/classes/action/class.xform.action_phpmailer.inc.php
+++ xform/classes/action/class.xform.action_phpmailer.inc.php
@@ -4,7 +4,7 @@
 * @link    http://rexdev.de/addons/xform-plugins.html
 * @link    http://redaxo.de/180-Addondetails.html?addon_id=319
 * @author  rexdev.de
-* @package redaxo4
+* @package XForm 2.8
 * @version Addon: 0.2
 * @version Klasse: 0.1
 *
@@ -18,16 +18,16 @@ class rex_xform_action_phpmailer extends rex_xform_action_abstract
   {
     global $REX;
 
-    $mail_from    = $this->getElement(2);                               //fb($mail_from,'INIT $mail_from');
-    $mail_to      = $this->getElement(3);                               //fb($mail_to,'INIT $mail_to');
+    $mail_from    = $this->getElement(2);
+    $mail_to      = $this->getElement(3);
     $mail_subject = $this->getElement(4);
     $mail_body    = $this->getElement(5);
 
 
     // RESOLVE ADRESSING
     ////////////////////////////////////////////////////////////////////////////
-    $mail_from    = $this->resolveMailFrom($mail_from);                         //fb($mail_from,'RESOLVED $mail_from');
-    $mail_to      = $this->resolveMailTo($mail_to);                             //fb($mail_to,'RESOLVED $mail_to');
+    $mail_from    = $this->resolveMailFrom($mail_from);
+    $mail_to      = $this->resolveMailTo($mail_to);
 
 
     // MAIL BODY TEMPLATE FROM ARTICLE OR SLICE
@@ -43,7 +43,7 @@ class rex_xform_action_phpmailer extends rex_xform_action_abstract
 
     // REPLACE VARS IN TEMPLATE
     ////////////////////////////////////////////////////////////////////////////
-    foreach ($this->params["value_pool"]["email"] as $search => $replace)
+    foreach ($this->params['value_pool']['email'] as $search => $replace)
     {
       $mail_body = str_replace('###'. $search .'###', $replace, $mail_body);
       $mail_body = str_replace('+++'. $search .'+++', urlencode($replace), $mail_body);
@@ -96,7 +96,6 @@ class rex_xform_action_phpmailer extends rex_xform_action_abstract
         }
       }
     }
-                                                                                //fb($mail,'$mail');
 
     $mail->Send();
 
@@ -107,17 +106,17 @@ class rex_xform_action_phpmailer extends rex_xform_action_abstract
 
   function getDescription()
   {
-    return '<strong>phpmailer</strong> : <em>Erweiterung der normalen "email" Klasse mit zusätzlichen features: mehrere Empfänger und Adressierungs-Typen (to,cc,bcc), Mailbody Bezug aus Article/Slice, Verarbeitung von Attachments</em><br />'.PHP_EOL
+    return '<strong>phpmailer</strong> : <em>Erweiterung der normalen "email" Klasse mit zus&auml;tzlichen features: mehrere Empf&auml;nger und Adressierungs-Typen (to,cc,bcc), Mailbody Bezug aus Article/Slice, Verarbeitung von Attachments</em><br />'.PHP_EOL
     .'Gewohnter Aufruf:<br />'.PHP_EOL
     .'<code class="xform-form-code">action|phpmailer|from@email.de|to@email.de|Mailsubject|Mailbody###name###"</code><br />'
-    .'Mehrere Empfänger:<br />'.PHP_EOL
+    .'Mehrere Empf&auml;nger:<br />'.PHP_EOL
     .'<code class="xform-form-code">action|phpmailer|from@email.de|to:a@bc.de,cc:a@bc.de,bcc:a@bc.de|Mailsubject|Mailbody###name###)"</code><br />'
     .'<em>Mailbody aus Slice beziehen -></em><br />'.PHP_EOL
     .'<code class="xform-form-code">action|phpmailer|from@email.de|to@email.de|Mailsubject|slice(123)"</code><br />'
     .'Mailbody aus Article beziehen:<br />'.PHP_EOL
     .'<code class="xform-form-code">action|phpmailer|from@email.de|to@email.de|Mailsubject|article(8,0)"</code><br />'
     .'Attachments:'.PHP_EOL
-    .'Daten eines uploads müssen von der jeiligen upload-Klasse aus $_FILES in xform_object->params["attachments"] als array hinterlegt worden sein:</em><br/>'.PHP_EOL
+    .'Daten eines uploads m&uuml;ssen von der jeiligen upload-Klasse aus $_FILES in xform_object->params["attachments"] als array hinterlegt worden sein:</em><br/>'.PHP_EOL
     .'<pre>"size"     => $_FILES["size"],
 "name"     => $_FILES["name"],
 "type"     => $_FILES["type"],
@@ -173,29 +172,29 @@ class rex_xform_action_phpmailer extends rex_xform_action_abstract
     if(strpos($mail_to,','))
     {
       // ARRAY OF RECIPIENTS
-      $tmp = explode(',',$mail_to);                                             //fb('ARRAY of recipients..');
+      $tmp = explode(',',$mail_to);
       foreach($tmp as $recipient)
       {
-        $r = explode(':',trim($recipient));                                     //fb($r,'$r');
+        $r = explode(':',trim($recipient));
 
         if(preg_match('/###.*###/',$r[1]))
         {
-          $r[1] = $this->resolvePlaceholder($r[1]);
+          $r[1] = self::resolvePlaceholder($r[1]);
         }
 
         $recipients[] = array('mode' => $r[0], 'email' => $r[1]);
       }
     }
     else
-    {                                                                           //fb('SINGLE recipients..');
+    {
       // SINGLE RECIPIENT
       if(strpos($mail_to,':'))
-      {                                                                         //fb('strpos($mail_to,":")..');
-        $mail_to = explode(':',$mail_to);                                       //fb($mail_to,'$mail_to');
+      {
+        $mail_to = explode(':',$mail_to);
 
         if(preg_match('/###.*###/',$mail_to[1]))
         {
-          $mail_to[1] = $this->resolvePlaceholder($mail_to[1]);
+          $mail_to[1] = self::resolvePlaceholder($mail_to[1]);
         }
 
         $recipients[] = array('mode' => $mail_to[0], 'email' => $mail_to[1]);
@@ -204,22 +203,20 @@ class rex_xform_action_phpmailer extends rex_xform_action_abstract
       {
         if(preg_match('/###.*###/',$mail_to))
         {
-          $mail_to = $this->resolvePlaceholder($mail_to);
+          $mail_to = self::resolvePlaceholder($mail_to);
         }
         $recipients[] = array('mode' => 'to', 'email' => $mail_to);
       }
-    }                                                                           //fb($recipients,'$recipients');
+    }
     return $recipients;
   }
 
 
   function resolveMailFrom($mail_from)
   {
-    if(preg_match('/###.*###/',$mail_from))
-    {
-      $mail_from = $this->resolvePlaceholder($mail_from);
-    }
-    return $mail_from;
+    return preg_match('/###.*###/',$mail_from)
+           ? self::resolvePlaceholder($mail_from)
+           : $mail_from;
   }
 
 
@@ -228,9 +225,9 @@ class rex_xform_action_phpmailer extends rex_xform_action_abstract
     global $REX;
 
     $placeholder = trim($placeholder,'###');
-    if(isset($this->elements_email[$placeholder]) && $this->elements_email[$placeholder] != '' )
+    if(isset($this->params['value_pool']['email'][$placeholder]) && $this->params['value_pool']['email'][$placeholder] != '' )
     {
-      return $this->elements_email[$placeholder];
+      return $this->params['value_pool']['email'][$placeholder];
     }
     else
     {
